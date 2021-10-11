26675aac37e4f463c57e2f0b5192e0ee0f26286c
Katie Pestotnik <katie.pestotnik@gmail.com>
2021-10-11 13:38:42 -0600

    Add translation ajax call

---
+++ public/app.js
@@ -0,0 +1,18 @@
+//entered text
+const $input = $('#userInput');
+//submit id
+const $translation = $("#translation");
+const translateFunction = (event) => {
+    event.preventDefault();
+    $.ajax({
+        url: `https://api-free.deepl.com/v2/translate?auth_key=3e2993ef-18a0-f0f4-1663-4ad7b859cfe9:fx&text=${$input.val()}&target_lang=JA`
+    }).then((data) => {
+        console.log(data.translations[0].text);
+        $translation.text(`${data.translations[0].text}`);
+    },
+        (error) => {
+            console.log(`AJAX problem`);
+        });
+};
+
+$('.translate-form').on('submit', translateFunction);
